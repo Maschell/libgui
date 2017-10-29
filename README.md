345bb53c6afe8333f6e8e85509fdc512c87c2ee1
Maschell <Maschell@gmx.de>
2017-10-29 10:28:14 +0100

    First commit, have fun!

---
+++ README.md
@@ -0,0 +1,51 @@
+# libgui
+
+
+## Usage
+Following steps are required for initialization:
+```
+InitOSFunctionPointers();   // Load OS functions
+InitPadScoreFunctionPointers();
+InitVPadFunctionPointers(); // Input functions for GUI control
+InitFSFunctionPointers();   // To load file from the SD Card
+InitGX2FunctionPointers();  // For rendering
+InitAXFunctionPointers();   // For sound
+
+mount_sd_fat("sd");			// Mounting the SD Card
+memoryInitialize();			// Initialize memory management
+
+//DO GUI STUFF HERE!
+
+memoryRelease();
+unmount_sd_fat("sd");
+```
+
+Link the application with:
+```
+-lgui -lutils -ldynamiclibs -lfreetype -lgd -lpng -ljpeg -lz  -lmad -lvorbisidec
+```
+
+TODO: provide more information
+
+## Dependencies
+To be able to use libgui, you need to install the following dependencies:
+
+- Application needs to be loaded from the [homebrew_launcher](https://github.com/dimok789/homebrew_launcher)
+- [libutils](https://github.com/Maschell/libutils) for common functions.
+- [dynamic_libs](https://github.com/Maschell/dynamic_libs/tree/lib) for access to the functions.
+
+And other portable libraries that can be found in the "libs" folder of this repository. Extract the "portlibs.zip" into your devkitPro directory.
+This package includes:
+
+- freetype2 
+- libgd 
+- libpng 
+- libjpeg 
+- libz  
+- libmad 
+- vorbisidec
+
+# Credits
+- Orignally based on https://github.com/dborth/libwiigui
+- Wii U port / modification / new functions / sound / much more by dimok.
+- Minor changes by Maschell
---
+++ source/resources/Resources.cpp
@@ -0,0 +1,208 @@
+
+#include <malloc.h>
+#include <string.h>
+#include "Resources.h"
+#include "filelist.h"
+#include <system/AsyncDeleter.h>
+#include <fs/FSUtils.h>
+#include "gui/GuiImageAsync.h"
+#include "gui/GuiSound.h"
+#include "utils/logger.h"
+
+Resources * Resources::instance = NULL;
+
+void Resources::Clear(){
+    ResourceFile * ResourceList = getResourceList();
+    if(ResourceList == NULL) return;
+
+	for(s32 i = 0; ResourceList[i].filename != NULL; ++i)
+	{
+		if(ResourceList[i].CustomFile)
+		{
+			free(ResourceList[i].CustomFile);
+			ResourceList[i].CustomFile = NULL;
+		}
+
+		if(ResourceList[i].CustomFileSize != 0)
+			ResourceList[i].CustomFileSize = 0;
+	}
+
+	if(instance)
+        delete instance;
+
+    instance = NULL;
+}
+
+bool Resources::LoadFiles(const char * path)
+{
+	if(!path)
+		return false;
+
+	bool result = false;
+	Clear();
+
+	ResourceFile * ResourceList = getResourceList();
+	if(ResourceList == NULL) return false;
+
+	for(s32 i = 0; ResourceList[i].filename != NULL; ++i)
+	{
+        std::string fullpath(path);
+        fullpath += "/";
+        fullpath += ResourceList[i].filename;
+
+        u8 * buffer = NULL;
+        u32 filesize = 0;
+
+        FSUtils::LoadFileToMem(fullpath.c_str(), &buffer, &filesize);
+
+        ResourceList[i].CustomFile = buffer;
+        ResourceList[i].CustomFileSize = (u32) filesize;
+        result |= (buffer != 0);
+	}
+
+	return result;
+}
+
+const u8 * Resources::GetFile(const char * filename)
+{
+    DEBUG_FUNCTION_LINE(".\n");
+    ResourceFile * ResourceList = getResourceList();
+    if(ResourceList == NULL) return NULL;
+
+	for(s32 i = 0; ResourceList[i].filename != NULL; ++i)
+	{
+		if(strcasecmp(filename, ResourceList[i].filename) == 0)
+		{
+			return (ResourceList[i].CustomFile ? ResourceList[i].CustomFile : ResourceList[i].DefaultFile);
+		}
+	}
+
+	return NULL;
+}
+
+u32 Resources::GetFileSize(const char * filename)
+{
+    ResourceFile * ResourceList = getResourceList();
+    if(ResourceList == NULL) return 0;
+
+	for(s32 i = 0; ResourceList[i].filename != NULL; ++i)
+	{
+		if(strcasecmp(filename, ResourceList[i].filename) == 0)
+		{
+			return (ResourceList[i].CustomFile ? ResourceList[i].CustomFileSize : ResourceList[i].DefaultFileSize);
+		}
+	}
+	return 0;
+}
+
+GuiImageData * Resources::GetImageData(const char * filename)
+{
+    if(!instance)
+        instance = new Resources;
+
+    ResourceFile * ResourceList = getResourceList();
+    if(ResourceList == NULL) return NULL;
+
+    std::map<std::string, std::pair<u32, GuiImageData *> >::iterator itr = instance->imageDataMap.find(std::string(filename));
+    if(itr != instance->imageDataMap.end())
+    {
+        itr->second.first++;
+        return itr->second.second;
+    }
+
+	for(s32 i = 0; ResourceList[i].filename != NULL; ++i)
+	{
+		if(strcasecmp(filename, ResourceList[i].filename) == 0)
+		{
+			const u8 * buff = ResourceList[i].CustomFile ? ResourceList[i].CustomFile : ResourceList[i].DefaultFile;
+			const u32 size = ResourceList[i].CustomFile ? ResourceList[i].CustomFileSize : ResourceList[i].DefaultFileSize;
+
+			if(buff == NULL)
+                return NULL;
+
+            GuiImageData * image = new GuiImageData(buff, size);
+            instance->imageDataMap[std::string(filename)].first = 1;
+            instance->imageDataMap[std::string(filename)].second = image;
+
+            return image;
+		}
+	}
+
+	return NULL;
+}
+
+void Resources::RemoveImageData(GuiImageData * image)
+{
+    std::map<std::string, std::pair<u32, GuiImageData *> >::iterator itr;
+
+    for(itr = instance->imageDataMap.begin(); itr != instance->imageDataMap.end(); itr++)
+    {
+        if(itr->second.second == image)
+        {
+            itr->second.first--;
+
+            if(itr->second.first == 0)
+            {
+                AsyncDeleter::pushForDelete( itr->second.second );
+                instance->imageDataMap.erase(itr);
+            }
+            break;
+        }
+    }
+}
+
+GuiSound * Resources::GetSound(const char * filename)
+{
+    if(!instance)
+        instance = new Resources;
+
+    std::map<std::string, std::pair<u32, GuiSound *> >::iterator itr = instance->soundDataMap.find(std::string(filename));
+    if(itr != instance->soundDataMap.end())
+    {
+        itr->second.first++;
+        return itr->second.second;
+    }
+
+    ResourceFile * ResourceList = getResourceList();
+    if(ResourceList == NULL) return NULL;
+
+	for(s32 i = 0; ResourceList[i].filename != NULL; ++i)
+	{
+		if(strcasecmp(filename, ResourceList[i].filename) == 0)
+		{
+			const u8 * buff = ResourceList[i].CustomFile ? ResourceList[i].CustomFile : ResourceList[i].DefaultFile;
+			const u32 size = ResourceList[i].CustomFile ? ResourceList[i].CustomFileSize : ResourceList[i].DefaultFileSize;
+
+			if(buff == NULL)
+                return NULL;
+
+            GuiSound * sound = new GuiSound(buff, size);
+            instance->soundDataMap[std::string(filename)].first = 1;
+            instance->soundDataMap[std::string(filename)].second = sound;
+
+            return sound;
+		}
+	}
+
+	return NULL;
+}
+
+void Resources::RemoveSound(GuiSound * sound)
+{
+    std::map<std::string, std::pair<u32, GuiSound *> >::iterator itr;
+
+    for(itr = instance->soundDataMap.begin(); itr != instance->soundDataMap.end(); itr++)
+    {
+        if(itr->second.second == sound)
+        {
+            itr->second.first--;
+
+            if(itr->second.first == 0)
+            {
+                AsyncDeleter::pushForDelete( itr->second.second );
+                instance->soundDataMap.erase(itr);
+            }
+            break;
+        }
+    }
+}
